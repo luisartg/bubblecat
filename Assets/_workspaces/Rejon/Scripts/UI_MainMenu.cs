6d4bb8776d09e75bc912dc5469a14d6d6aa246ea
edrejonuniat <122691476+edrejonuniat@users.noreply.github.com>
2025-01-22 20:48:23 -0600

    Se agrega TextmexPro

--- Assets/_workspaces/Rejon/Scripts/UI_MainMenu.cs
+++ Assets/_workspaces/Rejon/Scripts/UI_MainMenu.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UI_MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
