d6cc27567e947e190db51a493a45926d7c7930ae
edrejonuniat <122691476+edrejonuniat@users.noreply.github.com>
2025-01-25 15:35:38 -0600

    Animaciones configuradas

---
+++ Assets/_workspaces/Rejon/Scripts/UI_Scripts/UICollectableCounter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class UICollectableCounter : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI scoreText;
+    private int score = 0;
+    private CollectibleCounter collectibleCounter;
+
+
+    void Start()
+    {
+        // scoreText = GetComponent<TMPro.TextMeshProUGUI>();
+        collectibleCounter = FindFirstObjectByType<CollectibleCounter>();
+        UpdateScoreText();
+        
+    }
+
+    // public void AddPoint()
+    // {
+    //     score++;
+    //     UpdateScoreText();
+    // }
+
+    // public void SubtractPoint()
+    // {
+    //     score--;
+    //     UpdateScoreText();
+    // }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = $"{collectibleCounter.GetCollectedNumber()} / {collectibleCounter.GetTotalCollectibles()}";   
+    }
+}
